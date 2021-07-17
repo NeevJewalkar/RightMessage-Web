91f9561fcca82559c3af7f2588158d4f200378f5
NeevJewalkar <neevkar@icloud.com>
2021-07-17 19:34:11 +0530

    Frontend Done 📺

--- src/index.js
+++ src/index.js
@@ -6,13 +6,31 @@ let app = express()
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(favicon('/Users/neev/Documents/Nodejs/RightMessageWeb/src/assets/Icon.ico'))
+app.use(favicon('./assets/Icon.ico'))
 app.use(express.static(__dirname + '/assets'));
 
 const fetch = require('node-fetch')
 
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+app.get('/mark', (req, res) => {
+    res.render('mark', {status: '', message: ''})
+})
+
+app.post('/mark', (req, res) => {
+    console.log(req.body.spam)
+    res.render('mark', {status: 'Marked', message: req.body.spam})
+})
+
+app.get('/check', (req, res) => {
+    res.render('check', {status: '', message: ''})
+})
+
+app.post('/check', (req, res) => {
+    console.log(req.body.spam)
+    res.render('check', {status: '∞ people have marked this message as Spam', message: req.body.spam})
 })
 
 app.listen(8080)
